d8b72bd46fc66fa2ba96b34e73c761ac958f812d
Drepic26 <nickcfeldman@gmail.com>
2015-01-07 23:58:07 -0500

    Changed the way properties are filtered and started granite

---
+++ core/src/main/java/com/github/drepic26/couponcodes/core/util/CouponCodesProperties.java
@@ -0,0 +1,7 @@
+package com.github.drepic26.couponcodes.core.util;
+
+public class CouponCodesProperties {
+
+	public static String VERSION = "$version";
+
+}
---
+++ mods/granite/src/main/java/com/github/drepic26/couponcodes/granite/GranitePlugin.java
@@ -0,0 +1,8 @@
+package com.github.drepic26.couponcodes.granite;
+
+import org.granitemc.granite.api.plugin.Plugin;
+
+//@Plugin(id = "couponcodes", name = "CouponCodes", version = CouponCodesProperties.version)
+public class GranitePlugin {
+
+}
