81fce8beb3baa4da8b4d0ea40f00eb8c0b0a6089
Drepic26 <nickcfeldman@gmail.com>
2015-01-08 00:28:30 -0500

    Added beginnings of granite kinda got stuck trying to figure out how to do things. Gotta find some docs for granite.

--- mods/granite/src/main/java/com/github/drepic26/couponcodes/granite/GranitePlugin.java
+++ mods/granite/src/main/java/com/github/drepic26/couponcodes/granite/GranitePlugin.java
@@ -1,8 +1,19 @@
 package com.github.drepic26.couponcodes.granite;
 
+import org.granitemc.granite.api.plugin.OnEnable;
 import org.granitemc.granite.api.plugin.Plugin;
+import org.granitemc.granite.api.plugin.PluginContainer;
 
-//@Plugin(id = "couponcodes", name = "CouponCodes", version = CouponCodesProperties.version)
+import com.github.drepic26.couponcodes.core.ServerModTransformer;
+import com.github.drepic26.couponcodes.core.util.CouponCodesProperties;
+
+@Plugin(id = "couponcodes", name = "CouponCodes", version = CouponCodesProperties.VERSION)
 public class GranitePlugin {
 
+	private ServerModTransformer transformer = new GraniteServerModTransformer();
+
+	@OnEnable
+	public void onEnable(PluginContainer container) {
+
+	}
 }
