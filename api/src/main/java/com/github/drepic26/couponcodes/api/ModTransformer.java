c8f860913a8a7bac9f9998a7bd031c5416b23fcf
Drepic26 <nickcfeldman@gmail.com>
2015-01-24 19:33:35 -0500

    Fixed derps in transformer

--- api/src/main/java/com/github/drepic26/couponcodes/api/ModTransformer.java
+++ api/src/main/java/com/github/drepic26/couponcodes/api/ModTransformer.java
@@ -1,18 +1,11 @@
 package com.github.drepic26.couponcodes.api;
 
-import com.github.drepic26.couponcodes.api.coupon.CouponHandler;
 import com.github.drepic26.couponcodes.api.entity.Player;
 
 public interface ModTransformer {
 
-	/**
-	 * Gets the current instance of the CouponHandler
-	 * @return the current instance of the CouponHandler
-	 */
-	public CouponHandler getCouponHandler();
-
 	public void scheduleRunnable(Runnable runnable);
 
-	public Player getPlayer();
+	public Player getPlayer(String UUID);
 
 }
