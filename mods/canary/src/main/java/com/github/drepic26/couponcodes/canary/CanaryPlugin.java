d22a3e0a6109c0385e10b7add29c03af7439be43
Drepic26 <nickcfeldman@gmail.com>
2015-01-30 15:04:25 -0500

    Canary: Added metrics

--- mods/canary/src/main/java/com/github/drepic26/couponcodes/canary/CanaryPlugin.java
+++ mods/canary/src/main/java/com/github/drepic26/couponcodes/canary/CanaryPlugin.java
@@ -1,23 +1,26 @@
 package com.github.drepic26.couponcodes.canary;
 
+import java.io.IOException;
+
 import com.github.drepic26.couponcodes.api.CouponCodes;
 import com.github.drepic26.couponcodes.canary.config.CanaryConfigHandler;
 import com.github.drepic26.couponcodes.canary.coupon.CanaryCouponHandler;
 import com.github.drepic26.couponcodes.canary.database.CanaryDatabaseHandler;
 import com.github.drepic26.couponcodes.canary.listeners.CanaryListener;
+import com.github.drepic26.couponcodes.canary.metrics.CustomDataSender;
+import com.github.drepic26.couponcodes.canary.metrics.Metrics;
 import com.github.drepic26.couponcodes.core.commands.SimpleCommandHandler;
 import com.github.drepic26.couponcodes.core.event.SimpleEventHandler;
 
 import net.canarymod.Canary;
-import net.canarymod.commandsys.CanaryCommand;
 import net.canarymod.commandsys.CommandDependencyException;
-import net.canarymod.commandsys.CommandOwner;
 import net.canarymod.logger.Logman;
 import net.canarymod.plugin.Plugin;
 
 public class CanaryPlugin extends Plugin {
 
 	private Logman logger;
+	private Metrics metrics;
 
 	@Override
 	public boolean enable() {
@@ -30,6 +33,14 @@ public class CanaryPlugin extends Plugin {
 
 		CouponCodes.setDatabaseHandler(new CanaryDatabaseHandler());
 		CouponCodes.setCouponHandler(new CanaryCouponHandler());
+
+		if (CouponCodes.getConfigHandler().getUseMetrics()) {
+			try {
+				metrics = new Metrics(this);
+				CouponCodes.getModTransformer().scheduleRunnable(new CustomDataSender(metrics));
+				metrics.start();
+			} catch (IOException e) {}
+		}
 
 		try {
 			Canary.commands().registerCommands(new CanaryListener(), this, false);
---
+++ mods/canary/src/main/java/com/github/drepic26/couponcodes/canary/metrics/CustomDataSender.java
@@ -0,0 +1,54 @@
+package com.github.drepic26.couponcodes.canary.metrics;
+
+import com.github.drepic26.couponcodes.api.CouponCodes;
+import com.github.drepic26.couponcodes.api.coupon.CouponHandler;
+import com.github.drepic26.couponcodes.canary.metrics.Metrics.Graph;
+
+public class CustomDataSender implements Runnable {
+
+	private Metrics metrics;
+	private CouponHandler ch;
+
+	public CustomDataSender(Metrics metrics) {
+		this.metrics = metrics;
+		this.ch = CouponCodes.getCouponHandler();
+	}
+
+	@Override
+	public void run() {
+		// Coupon Types
+	    Graph couponTypesGraph = metrics.createGraph("Coupon Types");
+	    couponTypesGraph.addPlotter(new Metrics.Plotter("Item Coupons") {
+			@Override
+			public int getValue() {
+				return ch.getAmountOf("Item");
+			}
+		});
+	    couponTypesGraph.addPlotter(new Metrics.Plotter("Economy Coupons") {
+			@Override
+			public int getValue() {
+				return ch.getAmountOf("Economy");
+			}
+		});
+	    couponTypesGraph.addPlotter(new Metrics.Plotter("Rank Coupons") {
+			@Override
+			public int getValue() {
+				return ch.getAmountOf("Rank");
+			}
+		});
+	    couponTypesGraph.addPlotter(new Metrics.Plotter("Xp Coupons") {
+			@Override
+			public int getValue() {
+				return ch.getAmountOf("Xp");
+			}
+		});
+	    // Database types
+	    Graph dbTypesGraph = metrics.createGraph("Database Type");
+	    dbTypesGraph.addPlotter(new Metrics.Plotter(CouponCodes.getDatabaseHandler().getDatabaseType()) {
+			@Override
+			public int getValue() {
+				return 1;
+			}
+		});
+	}
+}
